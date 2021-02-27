603308479400b9dc7d8ce7b67162b0aecf88c5c8
Andrey Yamanov <tenphi@gmail.com>
2021-02-27 10:50:01 +0300

    fix notation bug

--- src/components/ColorsOutput.vue
+++ src/components/ColorsOutput.vue
@@ -1,39 +1,41 @@
 <template>
-  <nu-pane content="space-between">
-    <nu-attrs for="btn" padding="1x 2x"/>
-    <nu-btngroup use-radiogroup :value="notation" @input="notation = $event.detail" inline>
-      <nu-btn value="rgba">Rgba</nu-btn>
-      <nu-btn value="hex">Hex</nu-btn>
-      <nu-btn value="hsluv">HSLuv</nu-btn>
-    </nu-btngroup>
-    <nu-btn clear @tap="downloadAsJSON" padding=".5x 1x">
-      <nu-icon name="download-outline"/>
-      JSON
-    </nu-btn>
-  </nu-pane>
+  <nu-flow gap>
+    <nu-pane content="space-between">
+      <nu-attrs for="btn" padding="1x 2x"/>
+      <nu-btngroup use-radiogroup :value="notation" @input="notation = $event.detail" inline>
+        <nu-btn value="rgba">Rgba</nu-btn>
+        <nu-btn value="hex">Hex</nu-btn>
+        <nu-btn value="hsluv">HSLuv</nu-btn>
+      </nu-btngroup>
+      <nu-btn clear @tap="downloadAsJSON" padding=".5x 1x">
+        <nu-icon name="download-outline"/>
+        JSON
+      </nu-btn>
+    </nu-pane>
 
-  <nu-card fill="#white" color="#black" gap="4x">
-    <slot></slot>
-    <nu-attrs for="pane" text="sb"/>
+    <nu-card fill="#white" color="#black" gap="4x">
+      <slot></slot>
+      <nu-attrs for="pane" text="sb"/>
 
-    <template v-for="(theme, index) in props.data" :key="index">
-      <nu-flow gap="2x">
-        <nu-flow>
-          <nu-h5>{{ THEME_TITLES[index] }} variant</nu-h5>
-          <nu-line></nu-line>
+      <template v-for="(theme, index) in props.data" :key="index">
+        <nu-flow gap="2x">
+          <nu-flow>
+            <nu-h5>{{ THEME_TITLES[index] }} variant</nu-h5>
+            <nu-line></nu-line>
+          </nu-flow>
+          <nu-grid
+            columns="repeat(auto-fit, minmax(16x, 1fr))"
+            flow="row"
+            gap="2x 1x"
+            width="max 100%"
+            text="nowrap"
+          >
+            <Color v-for="color in theme" :data="color" :notation="notation"/>
+          </nu-grid>
         </nu-flow>
-        <nu-grid
-          columns="repeat(auto-fit, minmax(16x, 1fr))"
-          flow="row"
-          gap="2x 1x"
-          width="max 100%"
-          text="nowrap"
-        >
-          <Color v-for="color in theme" :data="color" :notation="notation"/>
-        </nu-grid>
-      </nu-flow>
-    </template>
-  </nu-card>
+      </template>
+    </nu-card>
+  </nu-flow>
 </template>
 
 <script setup>
